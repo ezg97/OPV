1e3d7031f0d0a8c1ecc0f2e79f5b395387105230
Elijah Guerrero <elijahguerrero97@gmail.com>
2021-05-25 15:31:18 -0500

    fixed navbar bottom highlight and media links hrefs

--- src/components/mod.media_links/media_links.js
+++ src/components/mod.media_links/media_links.js
@@ -6,25 +6,25 @@ function Media_Links(props) {
         <div className='media_links'>
             <ul>
                 <li>
-                    <a href="#">
+                    <a href="https://www.facebook.com/oilsporvida">
                         <i className="fa fa-facebook" aria-hidden="true"></i>
                         <span> - Facebook</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="https://twitter.com/oilsporvida">
                         <i className="fa fa-twitter" aria-hidden="true"></i>
                         <span> - Twitter</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="mailto:oilsporvida@gmail.com?subject=Directed%20from%20Oilsporvida">
                         <i className="fa fa-google-plus" aria-hidden="true"></i>
                         <span> - Google</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="https://www.instagram.com/oilsporvida/?hl=en">
                         <i className="fa fa-instagram" aria-hidden="true"></i>
                         <span> - Instagram</span>
                     </a>
